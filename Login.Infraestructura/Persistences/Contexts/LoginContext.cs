b49a07a741bb120b51c5d88b1ca9fec9c0abcd96
Johnny Herrera <jmjaramillo95@gmail.com>
2025-05-05 23:13:24 -0500

    context

---
+++ Login.Infraestructura/Persistences/Contexts/LoginContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Infraestructura.Persistences.Contexts
+{
+    internal class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options) 
+            : base(options)
+        { }
+    }
+}
