3fbeac40507890acb837657444bd43330b8da503
Johnny Herrera <jmjaramillo95@gmail.com>
2025-05-06 23:53:35 -0500

    entities-persona

---
+++ Login.Domain/Entities/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Domain.Entities
+{
+    public class Persona
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Persona { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Identificion { get; set; }
+        public string FechaNacimineto { get; set; }
+    }
+}
