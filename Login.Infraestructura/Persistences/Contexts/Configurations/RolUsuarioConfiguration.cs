18531ffc30cac66f704bab6a9198176d3eb627ee
Johnny Herrera <jmjaramillo95@gmail.com>
2025-05-18 23:53:52 -0500

    rolUsuarioConfigure

---
+++ Login.Infraestructura/Persistences/Contexts/Configurations/RolUsuarioConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Login.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Login.Infraestructura.Persistences.Contexts.Configurations
+{
+    internal class RolUsuarioConfiguration : IEntityTypeConfiguration<RolUsuario>
+    {
+        public void Configure(EntityTypeBuilder<RolUsuario> builder)
+        {
+            builder.HasKey(ru => new { ru.RolId, ru.UsuarioId });
+
+            builder.HasOne(ru => ru.Rol)
+                .WithMany(r => r.RolesUsuarios)
+                .HasForeignKey(ru => ru.RolId);
+
+            builder.HasOne(ru => ru.Usuario)
+                .WithMany(u => u.RolesUsuarios)
+                .HasForeignKey(ru => ru.UsuarioId);
+        }
+    }
+}
