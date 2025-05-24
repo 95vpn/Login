9d58d8f6022a5a70fea72c6454f5974fc3b30889
Johnny Herrera <jmjaramillo95@gmail.com>
2025-05-23 23:49:34 -0500

    injectionExtensions

---
+++ Login.Infraestructura/Extensions/InjectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Login.Infraestructura.Persistences.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Login.Infraestructura.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionInfraestura(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var assembly = typeof(LoginContext).Assembly.FullName;
+
+            services.AddDbContext<LoginContext>(
+                options => options.UseSqlServer(
+                        configuration.GetConnectionString("LoginConnection"), b => b.MigrationsAssembly(assembly)), ServiceLifetime.Transient);
+                    
+            return services;
+        }
+    }
+}
