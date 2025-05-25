62edf93e95d3c460a484c3f5b815c8b9baadd8b4
Johnny Herrera <jmjaramillo95@gmail.com>
2025-05-24 23:54:55 -0500

    add-services

--- Login.Api/Program.cs
+++ Login.Api/Program.cs
@@ -1,6 +1,11 @@
+using Login.Infraestructura.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddInjectionInfraestura(configuration);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
