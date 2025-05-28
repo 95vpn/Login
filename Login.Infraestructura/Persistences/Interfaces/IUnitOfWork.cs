68dc85d022237e9ab8abe680fb6164566ad8fb8c
Johnny Herrera <jmjaramillo95@gmail.com>
2025-05-27 23:21:04 -0500

    unitofwork

---
+++ Login.Infraestructura/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Infraestructura.Persistences.Interfaces
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        IUsuarioRepository Usuario { get; }
+    }
+}
