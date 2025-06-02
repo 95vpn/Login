056ac16f1edd4795d4dc746ee78d7ddd1b7b2fe6
Johnny Herrera <jmjaramillo95@gmail.com>
2025-06-01 22:20:57 -0500

    infraestructura-persistence

--- Login.Infraestructura/Persistences/Interfaces/IUsuarioRepository.cs
+++ Login.Infraestructura/Persistences/Interfaces/IUsuarioRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Login.Domain.Entities;
 
 namespace Login.Infraestructura.Persistences.Interfaces
 {
